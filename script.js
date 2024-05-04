e76276fbd690a96bfa85dc52ed73383837dd760c
Kelvin Lima <prokelvin65@gmail.com>
2024-05-04 13:55:53 -0300

    Criado divisórias de cada grupo de motos

--- script.js
+++ script.js
@@ -3,36 +3,38 @@ let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
 
 btnMenu.addEventListener('click', () => {
-    menu.classList.add('abrir-menu')
+  menu.classList.add('abrir-menu')
 })
 
 menu.addEventListener('click', () => {
-    menu.classList.remove('abrir-menu')
+  menu.classList.remove('abrir-menu')
 })
 
 overlay.addEventListener('click', () => {
-    menu.classList.remove('abrir-menu')
+  menu.classList.remove('abrir-menu')
 })
 
 
 var TrandingSlider = new Swiper('.tranding-slider', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    loop: true,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 2.5,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  });
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+
